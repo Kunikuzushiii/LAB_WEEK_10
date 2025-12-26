705e75516d1182cdf81de6d7355dcb633d14b5cc
Kunikuzushiii <qonita2307@gmail.com>
2025-12-26 17:45:06 +0700

    Commit No. 2: Implement LiveData to Sync Activity and Fragment UI

---
+++ app/src/main/java/com/example/lab_week_10/FirstFragment.kt
@@ -0,0 +1,32 @@
+package com.example.lab_week_10
+
+import android.os.Bundle
+import android.view.*
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        prepareViewModel()
+    }
+
+    private fun prepareViewModel(){
+        val viewModel = ViewModelProvider(requireActivity())[TotalViewModel::class.java]
+
+        viewModel.total.observe(viewLifecycleOwner) { total ->
+            updateText(total)
+        }
+    }
+
+    private fun updateText(total: Int) {
+        view?.findViewById<TextView>(R.id.text_total)?.text = getString(R.string.text_total, total)
+    }
+}
